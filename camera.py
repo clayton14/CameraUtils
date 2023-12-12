4be67e1ed28e8e3925d805ab4c4b5f20d5661d8f
clayton14 <claytoneasley77@gmail.com>
2023-12-11 22:43:32 -0500

    inherit Thread

--- camera.py
+++ camera.py
@@ -2,16 +2,22 @@ import cv2
 import os, sys
 from threading import Thread
 
-class Camera():
+class Camera(Thread):
     """
     Represents a camera connected to a computer 
     capable of spawing a new thread for each
     """
     def __init__(self, width:int, height:int, source:str) -> None:
+        super(Camera, self).__init__()
+        self.daemon = True
         self.wdth = width
         self.height = height
         (self.grabbed, self.frames) = cv2.VideoCapture(source)
         self.is_running = False
+
+        # TODO test to see if this works to set resloution of vido
+        # self.frames.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+        # self.frames.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 
     def start():
@@ -24,4 +30,5 @@ class Camera():
 
 
 if __name__ == "__main__":
-    pass
+    cam = Camera()
+    

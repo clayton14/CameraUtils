11655f08663063d19345194c2c94f42453121d07
clayton14 <claytoneasley77@gmail.com>
2023-12-11 23:30:24 -0500

    getting frames

--- camera.py
+++ camera.py
@@ -2,26 +2,39 @@ import cv2
 import os, sys
 from threading import Thread
 
+
 class Camera(Thread):
     """
     Represents a camera connected to a computer 
     capable of spawing a new thread for each
     """
-    def __init__(self, width:int, height:int, source:str) -> None:
+    def __init__(self, width:int, height:int, source:int) -> None:
         super(Camera, self).__init__()
         self.daemon = True
+        self.start()
         self.wdth = width
         self.height = height
-        (self.grabbed, self.frames) = cv2.VideoCapture(source)
-        self.is_running = False
+        self.capture = cv2.VideoCapture(source)
+        self.is_running = True
 
         # TODO test to see if this works to set resloution of vido
         # self.frames.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
         # self.frames.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
+    def __exit__(self):
+        self.capture.release()
 
-    def start():
-        pass
+
+
+    def get_frames(self):
+        try:
+            while self.capture.isOpened():
+                if self.is_running:
+                    (self.grabbed, self.frames) = self.capture.read()
+                    print(self.frames)
+        except KeyboardInterrupt as ex:
+            print("exit")
+
 
 
     def getStrema():
@@ -30,5 +43,8 @@ class Camera(Thread):
 
 
 if __name__ == "__main__":
-    cam = Camera()
+    cam = Camera(480, 620, 0)
+    cam.is_running = True
+    print(cam.name)
+    print(cam.get_frames())
     

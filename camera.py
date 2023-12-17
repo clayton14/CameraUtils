5a40f655be613dfc157a0da7c7f1ed6902e8702c
clayton14 <claytoneasley77@gmail.com>
2023-12-17 15:28:02 -0500

    add get_devices and remove inheritance

--- camera.py
+++ camera.py
@@ -9,19 +9,18 @@ class Camera(Thread):
     """
     Represents a vidoe device connected to a computer. 
     Spawns a new thread for reach camera stream and outputs numpy array 
-
     """
     def __init__(self, width:int, height:int, source:int ) -> None:
         super(Camera, self).__init__()
         self.daemon = True
-        self.capture = cv2.VideoCapture(source)
+        #self.capture = cv2.VideoCapture(source)
 
         self.wdth = width
         self.height = height
+        self.is_running = False
+        # self.grabbed = False
+ 
 
-        self.is_running = True
-        self.grabbed = False
-        self.frames = np.array(0)
         # TODO test to see if this works to set resloution of vido
         # self.frames.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
         # self.frames.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
@@ -32,15 +31,38 @@ class Camera(Thread):
         self.get_frames()
 
 
+    def get_devices(self):
+        # TODO - list all the camer deviecs and return list of avlaible devices
+        index = 0
+        cameras = []
+        try:
+            while True:
+                print("INDEX ", index)
+                cam = cv2.VideoCapture(f"/dev/video{index}")
+                print("IS OPENED ", cam.isOpened())
+                if cam.isOpened():
+                    check, _ = cam.read()
+                    print("CHECK " , check)
+                    if check:
+                        cameras.append(cam)
+                        print(f"Cmaera found on /dev/video{index}")
+                        index += 2
+                else:
+                    return cameras    
+        except Exception as e:
+            print(e)
+            
+
     def get_frames(self):
         # print(self.capure.isOpened())
         try:
             while self.capture.isOpened():
                 if self.is_running:
-                    (self.grabbed, self.frames) = self.capture.read()
-                    print(self.grabbed)
-                    print(f"{self.name}: ", self.frames)
-
+                    (grabbed, frames) = self.capture.read()
+                    print(frames)
+                    # print(self.grabbed)
+                    # print(f"{self.name}: ", self.frames)
+                    np.append(self.frames, frames)
         except KeyboardInterrupt as ex:
             print("exit")
             self.capture.release()
@@ -57,27 +79,17 @@ class Camera(Thread):
                 sys.exit("exiting")
         except KeyboardInterrupt as ex:
             print("exit")
+            self.capture.release()
+            cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
     #create a list of cameras
-    camers = [Camera(480, 620, 0), Camera(480, 620, 2)]
+    #camers = [Camera(480, 620, 0), Camera(480, 620, 2)]
     # start each camera in lisr
-    for cam in camers:
-        cam.start()
+    cam = Camera(480, 620, 0)
+    x = cam.get_devices()
+    print(x)
     
-    camers[0].get_frames()
-
-
-    # cam = Camera(480, 620, 0)
-    # cam.start()
-    # cam.is_running = True
-    # print(cam.name)
-    # print(cam.get_frames())
-
-    # cam_1 = Camera(480, 620, 2)
-    # cam.start()
-    # cam_1.is_running = True
-    # print(cam_1.name)
-    # print(cam_1.get_frames())
     
+
